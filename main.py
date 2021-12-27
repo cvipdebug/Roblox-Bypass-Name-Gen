7ff710ecf47a078baf0cda6e1e54060bec3990d3
cvip <80930896+ph-hub@users.noreply.github.com>
2021-12-26 17:23:23 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import random, string, requests, gratient
 from os import system
 
-from requests import api
-
 system("cls && title Cvip Roblox Bypass Name Gen")
 
 
@@ -17,6 +15,7 @@ def main():
     [3] Fuck
     [4] Bitch
     [5] Shit
+    [6] Ass
 
     """)
     print(logo_names)
@@ -111,6 +110,27 @@ def main():
             #the best is 2 up
             random_shit = ("").join(random.choice(string.ascii_letters)for k in range(2))
             EndPart = "it"
+            name = f"{firstPart}{random_shit}{EndPart}"
+            checker = requests.get(f"https://auth.roblox.com/v2/usernames/validate?request.username={name}&request.birthday=2000-01-01&request.context=Signup").json()
+            code1 = checker['message']
+
+            if code1 == "Username is valid":
+                woeking+=1
+                print(gratient.green(f"Working {name}"))
+                system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+                with open("Names.txt", "a") as f:
+                    f.write(f"{name}\n")
+                    f.close()
+            else:
+                notworking+=1
+                print(gratient.red(f"No Working {name}"))
+                system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+    if name_chose == "6":
+        while True:
+            firstPart = "as"
+            #the best is 3 up
+            random_shit = ("").join(random.choice(string.ascii_letters)for k in range(3))
+            EndPart = "s"
             name = f"{firstPart}{random_shit}{EndPart}"
             checker = requests.get(f"https://auth.roblox.com/v2/usernames/validate?request.username={name}&request.birthday=2000-01-01&request.context=Signup").json()
             code1 = checker['message']
