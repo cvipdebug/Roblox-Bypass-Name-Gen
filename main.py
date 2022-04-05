9c5ec975a1ed8adc25963f940f3b6423fb55b5b5
cvip <80930896+ph-hub@users.noreply.github.com>
2022-04-04 17:05:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,222 @@ proxies = open("proxy.txt").read().splitlines()
 def getProxy():
     return random.choice(proxies)
 
-    
+def assbeater420():
+    woeking = 0
+    notworking = 0
+
+    firstPart = "as"
+    #the best is 2 up
+    random_shit = ("").join(random.choice(string.ascii_letters)for k in range(2))
+    EndPart = "sBeater42I0"
+    name = f"{firstPart}{random_shit}{EndPart}"
+    checker = requests.get(f"https://auth.roblox.com/v2/usernames/validate?request.username={name}&request.birthday=2000-01-01&request.context=Signup", proxies={"http": 'http://' + getProxy()}).json()
+    code1 = checker['message']
+
+    if code1 == "Username is valid":
+        woeking+=1
+        print("["+Fore.GREEN+"INFO"+Fore.WHITE+f"] Working {name}")
+        system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+        with open("Names.txt", "a") as f:
+            f.write(f"{name}\n")
+            f.close()
+    else:
+        notworking+=1
+        print("["+Fore.RED+"INFO"+Fore.WHITE+f"] Not Working {name}")
+        system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+
+def nigger():
+    woeking = 0
+    notworking = 0
+
+    firstPart = "ni"
+    #the best is 2 up
+    random_shit = ("").join(random.choice(string.ascii_letters)for k in range(2))
+    EndPart = "gger"
+    name = f"{firstPart}{random_shit}{EndPart}"
+    checker = requests.get(f"https://auth.roblox.com/v2/usernames/validate?request.username={name}&request.birthday=2000-01-01&request.context=Signup", proxies={"http": 'http://' + getProxy()}).json()
+    code1 = checker['message']
+
+    if code1 == "Username is valid":
+        woeking+=1
+        print("["+Fore.GREEN+"INFO"+Fore.WHITE+f"] Working {name}")
+        system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+        with open("Names.txt", "a") as f:
+            f.write(f"{name}\n")
+            f.close()
+    else:
+        notworking+=1
+        print("["+Fore.RED+"INFO"+Fore.WHITE+f"] Not Working {name}")
+        system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+
+def Nigga():
+    woeking = 0
+    notworking = 0
+
+    firstPart = "ni"
+    #the best is 2 up
+    random_shit = ("").join(random.choice(string.ascii_letters)for k in range(2))
+    EndPart = "gga"
+    name = f"{firstPart}{random_shit}{EndPart}"
+    checker = requests.get(f"https://auth.roblox.com/v2/usernames/validate?request.username={name}&request.birthday=2000-01-01&request.context=Signup", proxies={"http": 'http://' + getProxy()}).json()
+    code1 = checker['message']
+
+    if code1 == "Username is valid":
+        woeking+=1
+        print("["+Fore.GREEN+"INFO"+Fore.WHITE+f"] Working {name}")
+        system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+        with open("Names.txt", "a") as f:
+            f.write(f"{name}\n")
+            f.close()
+    else:
+        notworking+=1
+        print("["+Fore.RED+"INFO"+Fore.WHITE+f"] Not Working {name}")
+        system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+
+def Fuck():
+    woeking = 0
+    notworking = 0
+
+    firstPart = "fu"
+    #the best is 2 up
+    random_shit = ("").join(random.choice(string.ascii_letters)for k in range(2))
+    EndPart = "qk"
+    name = f"{firstPart}{random_shit}{EndPart}"
+    checker = requests.get(f"https://auth.roblox.com/v2/usernames/validate?request.username={name}&request.birthday=2000-01-01&request.context=Signup", proxies={"http": 'http://' + getProxy()}).json()
+    code1 = checker['message']
+
+    if code1 == "Username is valid":
+        woeking+=1
+        print("["+Fore.GREEN+"INFO"+Fore.WHITE+f"] Working {name}")
+        system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+        with open("Names.txt", "a") as f:
+            f.write(f"{name}\n")
+            f.close()
+    else:
+        notworking+=1
+        print("["+Fore.RED+"INFO"+Fore.WHITE+f"] Not Working {name}")
+        system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+
+def Bitch():
+    woeking = 0
+    notworking = 0
+
+    firstPart = "bit"
+    #the best is 2 up
+    random_shit = ("").join(random.choice(string.ascii_letters)for k in range(2))
+    EndPart = "ch"
+    name = f"{firstPart}{random_shit}{EndPart}"
+    checker = requests.get(f"https://auth.roblox.com/v2/usernames/validate?request.username={name}&request.birthday=2000-01-01&request.context=Signup", proxies={"http": 'http://' + getProxy()}).json()
+    code1 = checker['message']
+
+    if code1 == "Username is valid":
+        woeking+=1
+        print("["+Fore.GREEN+"INFO"+Fore.WHITE+f"] Working {name}")
+        system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+        with open("Names.txt", "a") as f:
+            f.write(f"{name}\n")
+            f.close()
+    else:
+        notworking+=1
+        print("["+Fore.RED+"INFO"+Fore.WHITE+f"] Not Working {name}")
+        system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+
+def Shit():
+    woeking = 0
+    notworking = 0
+
+    firstPart = "sh"
+    #the best is 2 up
+    random_shit = ("").join(random.choice(string.ascii_letters)for k in range(2))
+    EndPart = "it"
+    name = f"{firstPart}{random_shit}{EndPart}"
+    checker = requests.get(f"https://auth.roblox.com/v2/usernames/validate?request.username={name}&request.birthday=2000-01-01&request.context=Signup", proxies={"http": 'http://' + getProxy()}).json()
+    code1 = checker['message']
+
+    if code1 == "Username is valid":
+        woeking+=1
+        print("["+Fore.GREEN+"INFO"+Fore.WHITE+f"] Working {name}")
+        system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+        with open("Names.txt", "a") as f:
+            f.write(f"{name}\n")
+            f.close()
+    else:
+        notworking+=1
+        print("["+Fore.RED+"INFO"+Fore.WHITE+f"] Not Working {name}")
+        system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+
+def Ass():
+    woeking = 0
+    notworking = 0
+
+    firstPart = "as"
+    #the best is 3 up
+    random_shit = ("").join(random.choice(string.ascii_letters)for k in range(3))
+    EndPart = "s"
+    name = f"{firstPart}{random_shit}{EndPart}"
+    checker = requests.get(f"https://auth.roblox.com/v2/usernames/validate?request.username={name}&request.birthday=2000-01-01&request.context=Signup", proxies={"http": 'http://' + getProxy()}).json()
+    code1 = checker['message']
+
+    if code1 == "Username is valid":
+        woeking+=1
+        print("["+Fore.GREEN+"INFO"+Fore.WHITE+f"] Working {name}")
+        system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+        with open("Names.txt", "a") as f:
+            f.write(f"{name}\n")
+            f.close()
+    else:
+        notworking+=1
+        print("["+Fore.RED+"INFO"+Fore.WHITE+f"] Not Working {name}")
+        system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+
+def GeorgeFloyd():
+    woeking = 0
+    notworking = 0
+
+    firstPart = "George"
+    #the best is 2 up
+    random_shit = ("").join(random.choice(string.ascii_letters)for k in range(2))
+    EndPart = "Floyd"
+    name = f"{firstPart}{random_shit}{EndPart}"
+    checker = requests.get(f"https://auth.roblox.com/v2/usernames/validate?request.username={name}&request.birthday=2000-01-01&request.context=Signup", proxies={"http": 'http://' + getProxy()}).json()
+    code1 = checker['message']
+
+    if code1 == "Username is valid":
+        woeking+=1
+        print("["+Fore.GREEN+"INFO"+Fore.WHITE+f"] Working {name}")
+        system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+        with open("Names.txt", "a") as f:
+            f.write(f"{name}\n")
+            f.close()
+    else:
+        notworking+=1
+        print("["+Fore.RED+"INFO"+Fore.WHITE+f"] Not Working {name}")
+        system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))        
+
+def Ishootniggasforfun():
+    woeking = 0
+    notworking = 0
+
+    firstPart = "ni"
+    #the best is 2 up
+    random_shit = ("").join(random.choice(string.ascii_letters)for k in range(2))
+    EndPart = "ggas"
+    name = f"{firstPart}{random_shit}{EndPart}"
+    checker = requests.get(f"https://auth.roblox.com/v2/usernames/validate?request.username=Ishoot{name}forfun&request.birthday=2000-01-01&request.context=Signup", proxies={"http": 'http://' + getProxy()}).json()
+    code1 = checker['message']
+
+    if code1 == "Username is valid":
+        woeking+=1
+        print("["+Fore.GREEN+"INFO"+Fore.WHITE+f"] Working Ishoot{name}forfun")
+        system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+        with open("Names.txt", "a") as f:
+            f.write(f"Ishoot{name}forfun\n")
+            f.close()
+    else:
+        notworking+=1
+        print("["+Fore.RED+"INFO"+Fore.WHITE+f"] Not Working Ishoot{name}forfun")
+        system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))        
+
 def main():
 
     woeking = 0
@@ -24,177 +239,49 @@ def main():
     [6] Ass
     [7] George Floyd
     [8] I shoot niggas for fun
+    [9] Ass Beater
+    [10] All
 
     """)
     print(logo_names)
     name_chose = input("What Name Do You Wnat?> ")
     if name_chose == "1":
         while True:
-            firstPart = "ni"
-            #the best is 2 up
-            random_shit = ("").join(random.choice(string.ascii_letters)for k in range(2))
-            EndPart = "gger"
-            name = f"{firstPart}{random_shit}{EndPart}"
-            checker = requests.get(f"https://auth.roblox.com/v2/usernames/validate?request.username={name}&request.birthday=2000-01-01&request.context=Signup", proxies={"http": 'http://' + getProxy()}).json()
-            code1 = checker['message']
-
-            if code1 == "Username is valid":
-                woeking+=1
-                print("["+Fore.GREEN+"INFO"+Fore.WHITE+f"] Working {name}")
-                system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
-                with open("Names.txt", "a") as f:
-                    f.write(f"{name}\n")
-                    f.close()
-            else:
-                notworking+=1
-                print("["+Fore.RED+"INFO"+Fore.WHITE+f"] Not Working {name}")
-                system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+            nigger()
     if name_chose == "2":
         while True:
-            firstPart = "ni"
-            #the best is 2 up
-            random_shit = ("").join(random.choice(string.ascii_letters)for k in range(2))
-            EndPart = "gga"
-            name = f"{firstPart}{random_shit}{EndPart}"
-            checker = requests.get(f"https://auth.roblox.com/v2/usernames/validate?request.username={name}&request.birthday=2000-01-01&request.context=Signup", proxies={"http": 'http://' + getProxy()}).json()
-            code1 = checker['message']
-
-            if code1 == "Username is valid":
-                woeking+=1
-                print("["+Fore.GREEN+"INFO"+Fore.WHITE+f"] Working {name}")
-                system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
-                with open("Names.txt", "a") as f:
-                    f.write(f"{name}\n")
-                    f.close()
-            else:
-                notworking+=1
-                print("["+Fore.RED+"INFO"+Fore.WHITE+f"] Not Working {name}")
-                system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+            Nigga()
     if name_chose == "3":
         while True:
-            firstPart = "fu"
-            #the best is 2 up
-            random_shit = ("").join(random.choice(string.ascii_letters)for k in range(2))
-            EndPart = "qk"
-            name = f"{firstPart}{random_shit}{EndPart}"
-            checker = requests.get(f"https://auth.roblox.com/v2/usernames/validate?request.username={name}&request.birthday=2000-01-01&request.context=Signup", proxies={"http": 'http://' + getProxy()}).json()
-            code1 = checker['message']
-
-            if code1 == "Username is valid":
-                woeking+=1
-                print("["+Fore.GREEN+"INFO"+Fore.WHITE+f"] Working {name}")
-                system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
-                with open("Names.txt", "a") as f:
-                    f.write(f"{name}\n")
-                    f.close()
-            else:
-                notworking+=1
-                print("["+Fore.RED+"INFO"+Fore.WHITE+f"] Not Working {name}")
-                system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+            Fuck()
     if name_chose == "4":
         while True:
-            firstPart = "bit"
-            #the best is 2 up
-            random_shit = ("").join(random.choice(string.ascii_letters)for k in range(2))
-            EndPart = "ch"
-            name = f"{firstPart}{random_shit}{EndPart}"
-            checker = requests.get(f"https://auth.roblox.com/v2/usernames/validate?request.username={name}&request.birthday=2000-01-01&request.context=Signup", proxies={"http": 'http://' + getProxy()}).json()
-            code1 = checker['message']
-
-            if code1 == "Username is valid":
-                woeking+=1
-                print("["+Fore.GREEN+"INFO"+Fore.WHITE+f"] Working {name}")
-                system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
-                with open("Names.txt", "a") as f:
-                    f.write(f"{name}\n")
-                    f.close()
-            else:
-                notworking+=1
-                print("["+Fore.RED+"INFO"+Fore.WHITE+f"] Not Working {name}")
-                system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+            Bitch()
     if name_chose == "5":
         while True:
-            firstPart = "sh"
-            #the best is 2 up
-            random_shit = ("").join(random.choice(string.ascii_letters)for k in range(2))
-            EndPart = "it"
-            name = f"{firstPart}{random_shit}{EndPart}"
-            checker = requests.get(f"https://auth.roblox.com/v2/usernames/validate?request.username={name}&request.birthday=2000-01-01&request.context=Signup", proxies={"http": 'http://' + getProxy()}).json()
-            code1 = checker['message']
-
-            if code1 == "Username is valid":
-                woeking+=1
-                print("["+Fore.GREEN+"INFO"+Fore.WHITE+f"] Working {name}")
-                system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
-                with open("Names.txt", "a") as f:
-                    f.write(f"{name}\n")
-                    f.close()
-            else:
-                notworking+=1
-                print("["+Fore.RED+"INFO"+Fore.WHITE+f"] Not Working {name}")
-                system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+            Shit()
     if name_chose == "6":
         while True:
-            firstPart = "as"
-            #the best is 3 up
-            random_shit = ("").join(random.choice(string.ascii_letters)for k in range(3))
-            EndPart = "s"
-            name = f"{firstPart}{random_shit}{EndPart}"
-            checker = requests.get(f"https://auth.roblox.com/v2/usernames/validate?request.username={name}&request.birthday=2000-01-01&request.context=Signup", proxies={"http": 'http://' + getProxy()}).json()
-            code1 = checker['message']
-
-            if code1 == "Username is valid":
-                woeking+=1
-                print("["+Fore.GREEN+"INFO"+Fore.WHITE+f"] Working {name}")
-                system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
-                with open("Names.txt", "a") as f:
-                    f.write(f"{name}\n")
-                    f.close()
-            else:
-                notworking+=1
-                print("["+Fore.RED+"INFO"+Fore.WHITE+f"] Not Working {name}")
-                system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+            Ass()
     if name_chose == "7":
         while True:
-            firstPart = "George"
-            #the best is 2 up
-            random_shit = ("").join(random.choice(string.ascii_letters)for k in range(2))
-            EndPart = "Floyd"
-            name = f"{firstPart}{random_shit}{EndPart}"
-            checker = requests.get(f"https://auth.roblox.com/v2/usernames/validate?request.username={name}&request.birthday=2000-01-01&request.context=Signup", proxies={"http": 'http://' + getProxy()}).json()
-            code1 = checker['message']
-
-            if code1 == "Username is valid":
-                woeking+=1
-                print("["+Fore.GREEN+"INFO"+Fore.WHITE+f"] Working {name}")
-                system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
-                with open("Names.txt", "a") as f:
-                    f.write(f"{name}\n")
-                    f.close()
-            else:
-                notworking+=1
-                print("["+Fore.RED+"INFO"+Fore.WHITE+f"] Not Working {name}")
-                system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+            GeorgeFloyd()
     if name_chose == "8":
         while True:
-            firstPart = "ni"
-            #the best is 2 up
-            random_shit = ("").join(random.choice(string.ascii_letters)for k in range(2))
-            EndPart = "ggas"
-            name = f"{firstPart}{random_shit}{EndPart}"
-            checker = requests.get(f"https://auth.roblox.com/v2/usernames/validate?request.username=Ishoot{name}forfun&request.birthday=2000-01-01&request.context=Signup", proxies={"http": 'http://' + getProxy()}).json()
-            code1 = checker['message']
-
-            if code1 == "Username is valid":
-                woeking+=1
-                print("["+Fore.GREEN+"INFO"+Fore.WHITE+f"] Working Ishoot{name}forfun")
-                system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
-                with open("Names.txt", "a") as f:
-                    f.write(f"Ishoot{name}forfun\n")
-                    f.close()
-            else:
-                notworking+=1
-                print("["+Fore.RED+"INFO"+Fore.WHITE+f"] Not Working Ishoot{name}forfun")
-                system("title Working: "+ str(woeking)+ " Not Working: "+str(notworking))
+            Ishootniggasforfun()
+    if name_chose == "9":
+        while True:
+            assbeater420()
+    if name_chose == "10":
+        while True:
+            nigger()
+            Nigga()
+            Fuck()
+            Bitch()
+            Shit()
+            Ass()
+            GeorgeFloyd()
+            Ishootniggasforfun()
+            assbeater420()
 
 threading.Thread(target= main()).start()
